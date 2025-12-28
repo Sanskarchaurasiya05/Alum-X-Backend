76ef29bd826e9dea208101b464d2f0874f429621
LooninS <hushed55@proton.me>
2025-12-28 23:08:41 +0530

    Issue: #87

---
+++ src/main/java/com/opencode/alumxbackend/Animal/Penguin.java
@@ -0,0 +1,14 @@
+public class Tiger {
+
+    public String makeSound() {
+        return "Honk!";
+    }
+
+    public String habitat() {
+        return "Antarctica";
+    }
+
+    public int numberOfLegs() {
+        return 2;
+    }
+}
