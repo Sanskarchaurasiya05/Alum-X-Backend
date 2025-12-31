29cf181d38ae827b0999247aa31fbb1bbc6f819d
suzzzal <kshatrisujal@gmail.com>
2025-12-31 14:28:03 +0530

    Update HouseApplication.java

--- src/main/java/com/opencode/alumxbackend/basics/suzzzal/house/HouseApplication.java
+++ src/main/java/com/opencode/alumxbackend/basics/suzzzal/house/HouseApplication.java
@@ -1,13 +1,1 @@
-package com.opencode.alumxbackend.basics.suzzzal.house;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HouseApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HouseApplication.class, args);
-    }
-
-}
